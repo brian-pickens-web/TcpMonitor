8131530fee3088b201c5262a81cf63e4952470d9
brian pickens <brian.pickens@brianpickensweb.com>
2020-07-22 23:10:06 -0500

    Initial Project Setup

---
+++ src/TcpMonitor/Views/App.cs
@@ -0,0 +1,19 @@
+﻿using Terminal.Gui;
+
+namespace TcpMonitor.Views
+{
+    public sealed class App : Toplevel
+    {
+        private View _menuBar = new MenuBar(new[]
+        {
+            new MenuBarItem("_File", new[] { new MenuItem("_Quit", string.Empty, Application.RequestStop) })
+        });
+
+        public App(MainView mainView)
+        {
+            Add(_menuBar);
+            mainView.Y = 1;
+            Add(mainView);
+        }
+    }
+}
