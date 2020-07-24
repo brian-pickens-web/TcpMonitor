26d3d3cab7113a369600a5ba08d6483729af396c
brian pickens <brian.pickens@brianpickensweb.com>
2020-07-24 01:06:38 -0500

    - Add TcpConnectionService and supporting classes

---
+++ src/TcpMonitor/Models/TcpConnectionModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace TcpMonitor.Models
+{
+    public class TcpConnectionModel
+    {
+        public uint ProcessId { get; set; }
+        public IPAddress LocalAddress { get; set; }
+        public ushort LocalPort { get; set; }
+        public IPAddress RemoteAddress { get; set; }
+        public ushort RemotePort { get; set; }
+        public string State { get; set; }
+    }
+}
---
+++ src/TcpMonitor/Services/WinTcpTable.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+
+namespace TcpMonitor.Services
+{
+    /// <summary>
+    /// Source: https://www.pinvoke.net/default.aspx/iphlpapi/getextendedtcptable.html
+    /// </summary>
+    public class WinTcpTable
+    {
+        [DllImport("iphlpapi.dll", SetLastError = true)]
+        static extern uint GetExtendedTcpTable(IntPtr pTcpTable, ref int dwOutBufLen, bool sort, int ipVersion, TcpTableClassType tblClassType, uint reserved = 0);
+
+        public const int AF_INET = 2;    // IP_v4 = System.Net.Sockets.AddressFamily.InterNetwork
+        public const int AF_INET6 = 23;  // IP_v6 = System.Net.Sockets.AddressFamily.InterNetworkV6
+
+        public static List<TcpRow> GetAllTCPConnections()
+        {
+            return GetTCPConnections<TcpRow, TcpTableOwner>(AF_INET);
+        }
+
+        public static List<Tcp6Row> GetAllTCPv6Connections()
+        {
+            return GetTCPConnections<Tcp6Row, Tcp6TableOwner>(AF_INET6);
+        }
+
+        private static List<IPR> GetTCPConnections<IPR, IPT>(int ipVersion)//IPR = Row Type, IPT = Table Type
+        {
+            IPR[] tableRows;
+            int buffSize = 0;
+
+            var dwNumEntriesField = typeof(IPT).GetField("dwNumEntries");
+
+            // how much memory do we need?
+            uint ret = GetExtendedTcpTable(IntPtr.Zero, ref buffSize, true, ipVersion, TcpTableClassType.TCP_TABLE_OWNER_PID_ALL);
+            IntPtr tcpTablePtr = Marshal.AllocHGlobal(buffSize);
+
+            try
+            {
+                ret = GetExtendedTcpTable(tcpTablePtr, ref buffSize, true, ipVersion, TcpTableClassType.TCP_TABLE_OWNER_PID_ALL);
+                if (ret != 0)
+                    return new List<IPR>();
+
+                // get the number of entries in the table
+                IPT table = (IPT)Marshal.PtrToStructure(tcpTablePtr, typeof(IPT));
+                int rowStructSize = Marshal.SizeOf(typeof(IPR));
+                uint numEntries = (uint)dwNumEntriesField.GetValue(table);
+
+                // buffer we will be returning
+                tableRows = new IPR[numEntries];
+
+                IntPtr rowPtr = (IntPtr)((long)tcpTablePtr + 4);
+                for (int i = 0; i < numEntries; i++)
+                {
+                    IPR tcpRow = (IPR)Marshal.PtrToStructure(rowPtr, typeof(IPR));
+                    tableRows[i] = tcpRow;
+                    rowPtr = (IntPtr)((long)rowPtr + rowStructSize);   // next entry
+                }
+            }
+            finally
+            {
+                // Free the Memory
+                Marshal.FreeHGlobal(tcpTablePtr);
+            }
+            return tableRows != null ? tableRows.ToList() : new List<IPR>();
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct TcpRow
+    {
+        public uint state;
+        public uint localAddr;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] localPort;
+        public uint remoteAddr;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] remotePort;
+        public uint owningPid;
+
+        public uint ProcessId
+        {
+            get { return owningPid; }
+        }
+
+        public IPAddress LocalAddress
+        {
+            get { return new IPAddress(localAddr); }
+        }
+
+        public ushort LocalPort
+        {
+            get
+            {
+                return BitConverter.ToUInt16(new byte[2] { localPort[1], localPort[0] }, 0);
+            }
+        }
+
+        public IPAddress RemoteAddress
+        {
+            get { return new IPAddress(remoteAddr); }
+        }
+
+        public ushort RemotePort
+        {
+            get
+            {
+                return BitConverter.ToUInt16(new byte[2] { remotePort[1], remotePort[0] }, 0);
+            }
+        }
+
+        public TcpState State
+        {
+            get { return (TcpState)state; }
+        }
+    }
+
+    public enum TcpState
+    {
+        MIB_TCP_STATE_CLOSED = 1,
+        MIB_TCP_STATE_LISTEN = 2,
+        MIB_TCP_STATE_SYN_SENT = 3,
+        MIB_TCP_STATE_SYN_RCVD = 4,
+        MIB_TCP_STATE_ESTAB = 5,
+        MIB_TCP_STATE_FIN_WAIT1 = 6,
+        MIB_TCP_STATE_FIN_WAIT2 = 7,
+        MIB_TCP_STATE_CLOSE_WAIT = 8,
+        MIB_TCP_STATE_CLOSING = 9,
+        MIB_TCP_STATE_LAST_ACK = 10,
+        MIB_TCP_STATE_TIME_WAIT = 11,
+        MIB_TCP_STATE_DELETE_TCB = 12
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct TcpTableOwner
+    {
+        public uint dwNumEntries;
+        [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.Struct, SizeConst = 1)]
+        public TcpRow[] table;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Tcp6Row
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        public byte[] localAddr;
+        public uint localScopeId;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] localPort;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        public byte[] remoteAddr;
+        public uint remoteScopeId;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+        public byte[] remotePort;
+        public uint state;
+        public uint owningPid;
+
+        public uint ProcessId
+        {
+            get { return owningPid; }
+        }
+
+        public long LocalScopeId
+        {
+            get { return localScopeId; }
+        }
+
+        public IPAddress LocalAddress
+        {
+            get { return new IPAddress(localAddr, LocalScopeId); }
+        }
+
+        public ushort LocalPort
+        {
+            get { return BitConverter.ToUInt16(localPort.Take(2).Reverse().ToArray(), 0); }
+        }
+
+        public long RemoteScopeId
+        {
+            get { return remoteScopeId; }
+        }
+
+        public IPAddress RemoteAddress
+        {
+            get { return new IPAddress(remoteAddr, RemoteScopeId); }
+        }
+
+        public ushort RemotePort
+        {
+            get { return BitConverter.ToUInt16(remotePort.Take(2).Reverse().ToArray(), 0); }
+        }
+
+        public TcpState State
+        {
+            get { return (TcpState)state; }
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Tcp6TableOwner
+    {
+        public uint dwNumEntries;
+        [MarshalAs(UnmanagedType.ByValArray, ArraySubType = UnmanagedType.Struct, SizeConst = 1)]
+        public Tcp6Row[] table;
+    }
+
+    public enum TcpTableClassType
+    {
+        TCP_TABLE_BASIC_LISTENER,
+        TCP_TABLE_BASIC_CONNECTIONS,
+        TCP_TABLE_BASIC_ALL,
+        TCP_TABLE_OWNER_PID_LISTENER,
+        TCP_TABLE_OWNER_PID_CONNECTIONS,
+        TCP_TABLE_OWNER_PID_ALL,
+        TCP_TABLE_OWNER_MODULE_LISTENER,
+        TCP_TABLE_OWNER_MODULE_CONNECTIONS,
+        TCP_TABLE_OWNER_MODULE_ALL
+    }
+}
