5800e58d4ffc19176c5c03afbb5d7059c4967b78
brian pickens <brian.pickens@brianpickensweb.com>
2020-07-25 12:35:40 -0500

    Move tcp connections grid into a dedicated scrollable view

--- src/TcpMonitor/Views/MainView.cs
+++ src/TcpMonitor/Views/MainView.cs
@@ -9,33 +9,13 @@ namespace TcpMonitor.Views
 {
     public sealed class MainView : Window
     {
-        private readonly GridView _tcpConnectionsGrid;
-
         public MainView()
         {
             Width = Dim.Fill();
             Height = Dim.Fill();
 
-            _tcpConnectionsGrid = new GridView();
-            // _tcpConnectionsGrid.SetDataSource(GetTcpConnections());
-            _tcpConnectionsGrid.SetRefreshableDataSource(RefreshTcpConnectionsGrid);
-            // Add(_tcpConnectionsGrid);
-        }
-
-        private IEnumerable<TcpConnectionModel> RefreshTcpConnectionsGrid()
-        {
-            Remove(_tcpConnectionsGrid);
-            foreach (var tcpConnectionModel in GetTcpConnections())
-            {
-                yield return tcpConnectionModel;
-            }
-            Add(_tcpConnectionsGrid);
-        }
-
-        private IEnumerable<TcpConnectionModel> GetTcpConnections()
-        {
-            return TcpConnectionService.GetTcpConnectionData()
-                .OrderBy(model => model.ProcessId);
+            var tcpConnectionsGrid = new TcpConnectionsGrid();
+            Add(tcpConnectionsGrid);
         }
     }
 }
--- src/TcpMonitor/Views/TcpConnectionsGrid.cs
+++ src/TcpMonitor/Views/TcpConnectionsGrid.cs
@@ -1,10 +1,41 @@
-﻿using TcpMonitor.Views.Common;
+﻿using System.Collections.Generic;
+using System.Linq;
+using TcpMonitor.Models;
+using TcpMonitor.Services;
+using TcpMonitor.Views.Common;
 using Terminal.Gui;
 
 namespace TcpMonitor.Views
 {
-    public class TcpConnectionsGrid : View
+    public class TcpConnectionsGrid : ScrollView
     {
+        private readonly GridView _tcpConnectionsGrid;
 
+        public TcpConnectionsGrid()
+            : base(new Rect(0, 0, 100, 25))
+        {
+            ShowVerticalScrollIndicator = true;
+            _tcpConnectionsGrid = new GridView();
+            _tcpConnectionsGrid.SetRefreshableDataSource(RefreshTcpConnectionsGrid);
+        }
+
+        private IEnumerable<TcpConnectionModel> RefreshTcpConnectionsGrid()
+        {
+            Remove(_tcpConnectionsGrid);
+            ContentSize = new Size(99, 2);
+            foreach (var tcpConnectionModel in GetTcpConnections())
+            {
+                ContentSize = Size.Add(ContentSize, new Size(0, 1));
+                yield return tcpConnectionModel;
+            }
+            Add(_tcpConnectionsGrid);
+        }
+
+        private IEnumerable<TcpConnectionModel> GetTcpConnections()
+        {
+            return TcpConnectionService.GetTcpConnectionData()
+                .OrderBy(model => model.ProcessId);
+        }
     }
+
 }
