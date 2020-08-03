1947c059b5022261546582040710eb6d372540b3
brian pickens <brian.pickens@brianpickensweb.com>
2020-08-03 10:56:52 -0500

    Reordered the TcpPerformanceModel properties for display purposes

--- src/TcpMonitor/Models/TcpPerformanceModel.cs
+++ src/TcpMonitor/Models/TcpPerformanceModel.cs
@@ -2,10 +2,10 @@
 {
     public class TcpPerformanceModel
     {
-        public int ConnectionFailures { get; set; }
         public int ConnectionsActive { get; set; }
         public int ConnectionsEstablished { get; set; }
         public int ConnectionsPassive { get; set; }
         public int ConnectionsReset { get; set; }
+        public int ConnectionFailures { get; set; }
     }
 }
