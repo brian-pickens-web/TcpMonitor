90ef968f3b52f07f29811b25755b0a1ca50d8702
brian pickens <brian.pickens@brianpickensweb.com>
2020-08-03 10:57:26 -0500

    Display the version of Windows running the app

--- src/TcpMonitor/Views/DashboardView.cs
+++ src/TcpMonitor/Views/DashboardView.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Microsoft.Win32;
 using TcpMonitor.Views.Framework;
 using TcpMonitor.Views.GridViews;
 using Terminal.Gui;
@@ -7,6 +8,21 @@ namespace TcpMonitor.Views
 {
     public sealed class DashboardView : View, IVisibilityChanged
     {
+        private const char DividerChar = '-';
+        private const string WINDOWS_VERSION_REGKEY = @"HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion";
+        private const string WINDOWS_PRODUCT_REGVALUE = @"ProductName";
+        private const string WINDOWS_RELEASE_REGVALUE = @"ReleaseId";
+
+        private static readonly string WindowsProduct = Registry.GetValue(
+            WINDOWS_VERSION_REGKEY, 
+            WINDOWS_PRODUCT_REGVALUE, 
+            string.Empty).ToString();
+
+        private static readonly string WindowsRelease = Registry.GetValue(
+            WINDOWS_VERSION_REGKEY, 
+            WINDOWS_RELEASE_REGVALUE, 
+            string.Empty).ToString();
+
         private readonly Action<bool> VisibilityChangedEvent;
 
         public DashboardView(TcpSettingsGrid tcpSettingsGrid, TcpPerformanceGrid tcpPerformanceGrid)
@@ -15,9 +31,22 @@ namespace TcpMonitor.Views
             Height = Dim.Fill();
             VisibilityChangedEvent += tcpPerformanceGrid.VisibilityChanged;
 
-            tcpSettingsGrid.Y = 0;
+            var colorScheme = new ColorScheme()
+            {
+                Normal = new Terminal.Gui.Attribute(Color.Black, Color.Blue)
+            };
+
+            var windowsVersion = $"{WindowsProduct} {WindowsRelease}";
+            var divider = string.Empty.PadLeft(windowsVersion.Length, DividerChar);
+
+            var windowsVersionLabel = new Label(windowsVersion) { Y = 0, ColorScheme = colorScheme };
+            var dividerLabel = new Label(divider) { Y = 1, ColorScheme = colorScheme };
+
+            tcpSettingsGrid.Y = Pos.Bottom(dividerLabel) + 1;
             tcpPerformanceGrid.Y = Pos.Bottom(tcpSettingsGrid) + 1;
 
+            Add(windowsVersionLabel);
+            Add(dividerLabel);
             Add(tcpSettingsGrid);
             Add(tcpPerformanceGrid);
         }
